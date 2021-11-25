55e95e805415dcdacf22cf5c5604d614d4fefd1e
charlie <viacharles@gmail.com>
2021-11-25 23:37:54 +0800

    feature(menu): module switch completed

--- src/app/modules/layout/components/menu/menu.component.ts
+++ src/app/modules/layout/components/menu/menu.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { EModule } from '@utility/enum/route.enum';
+import { MenuMap } from '@utility/map/router.map';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+  @Output() moduleChanged = new EventEmitter<EModule>();
+  @Input() module: EModule = EModule.User;
+  constructor() {}
+  public readonly Menu = MenuMap;
 }
--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationService } from '@shared/services/navigation.service';
+import { EModule } from '@utility/enum/route.enum';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-
-  constructor(
-    public $navigation: NavigationService
-  ) { }
-
-  ngOnInit(): void {
+  constructor(public $navigation: NavigationService) {}
+  get Module(): typeof EModule {
+    return EModule;
   }
+  public module: EModule = EModule.User;
 
+  ngOnInit(): void {}
 }
