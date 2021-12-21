29dba26a80ec35e189174f8ee75e0817c298fa7b
charlie <viacharles@gmail.com>
2021-12-21 22:05:27 +0800

    deploy to github page

--- src/app/modules/business/business-routing.module.ts
+++ src/app/modules/business/business-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Module, Page } from '@utility/enum/route.enum';
-import { getPageMap } from '@utility/map/router.map';
 
-const routes: Routes = [{
-  path: Page.FCoin,
-  loadChildren: () => import('./coining/coining.module').then(m => m.CoiningModule)
-}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BusinessRoutingModule { }
+export class BusinessRoutingModule {}
