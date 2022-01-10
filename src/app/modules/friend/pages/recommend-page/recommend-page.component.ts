1c472b8a0865637e1cee28b97d9b8433331bedea
charlie <viacharles@gmail.com>
2022-01-10 21:38:03 +0800

    fix(recommend-page) 用service方法組合重構

--- src/app/modules/friend/pages/recommend-page/recommend-page.component.ts
+++ src/app/modules/friend/pages/recommend-page/recommend-page.component.ts
@@ -1,10 +1,12 @@
+import { take } from 'rxjs/operators';
 import { LoggerService } from '@shared/services/logger.service';
-import { FriendService } from './../../shared/services/friend.service';
-import { IFriend } from '@utility/interface/user.interface';
-import { FirebaseService } from '@shared/services/firebase.service';
-import { UserService } from '@user/shared/services/user.service';
+import { IUser } from '@utility/interface/user.interface';
 import { Component, OnInit } from '@angular/core';
-import { FriendsAction } from '@user/shared/models/friend.model';
+import { FriendsAction as Action } from '@user/shared/models/friend.model';
+import { environment } from 'src/environments/environment';
+import { UserService } from '@user/shared/services/user.service';
+import { User } from '@user/shared/models/user.model';
+import { FriendService } from '../../shared/services/friend.service';
 
 @Component({
   selector: 'app-recommend-page',
@@ -13,25 +15,56 @@ import { FriendsAction } from '@user/shared/models/friend.model';
 })
 export class RecommendPageComponent implements OnInit {
 
-  constructor(private $user: UserService, private $fb: FirebaseService, private $friend: FriendService, private $logger: LoggerService) { }
+  constructor(
+    private $feature: FriendService,
+    private $user: UserService,
+    private $logger: LoggerService) { }
+
+  public inviteList: IUser[] = [];
+  public recommendList: IUser[] = [];
+  public defaultAvatar = environment.defaultAvatar;
+  private user: User | null = null;
+
+  get id(): string {
+    return (this.user as User).id;
+  }
 
   ngOnInit(): void {
+    this.$user.user$.pipe(
+      take(1)
+    ).subscribe(
+      user => {
+        this.user = user;
+        this.init();
+      }
+    );
     this.init();
   }
 
-  public inviteList: {key: IFriend}[] = [];
-
-
+  public addFriend(friendId: string): void {
+    this.$feature.fireEvent({
+      action: Action.AddFriend,
+      id: this.id,
+      friendId
+    });
+  }
 
   private init(): void {
-    this.$friend.fireEvent({
-      action: FriendsAction.FetchInviteList,
-      id: sessionStorage.getItem('id') as string
-    }).then((friends: any) => {
-      console.log('FriendsAction', friends)
-      this.$logger.systemMessage(`${(<[]>friends).length} invitations were received `);
+    this.$feature.fireEvent<IUser[]>({
+      action: Action.FetchInviteList,
+      id: this.id
+    }).then((friends: IUser[]) => {
+      this.$logger.systemMessage(`${(friends as []).length} invitations were received `);
       this.inviteList = friends;
-    })
+    });
+    this.$feature.fireEvent<IUser[]>({
+      action: Action.FetchRecommendList,
+      id: this.id
+    }).then((friends: IUser[]) => {
+      this.$logger.systemMessage(`${(friends as []).length} recommendations were received `);
+      this.recommendList = friends;
+    }
+    );
   }
 
 }
--- src/app/modules/friend/shared/services/friend.service.ts
+++ src/app/modules/friend/shared/services/friend.service.ts
@@ -1,65 +1,107 @@
-import { filter } from 'rxjs/operators';
+import { filter, map, take } from 'rxjs/operators';
 import { FirebaseService } from '@shared/services/firebase.service';
 import { LoggerService } from '@shared/services/logger.service';
 import { FeatureService } from '@utility/abstract/feature-service.abstract';
-import { UserService } from '@user/shared/services/user.service';
 import { Injectable } from '@angular/core';
 import { FriendsAction as Action, FriendsAction, IFriendsEvent } from '@user/shared/models/friend.model';
 import { IFriend, IUser } from '@utility/interface/user.interface';
 import { forkJoin } from 'rxjs';
+import { UserService } from '@user/shared/services/user.service';
+import { User } from '@user/shared/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FriendService extends FeatureService<IFriendsEvent, FriendsAction> {
 
-  constructor(private $fb: FirebaseService, protected $logger: LoggerService) { super( $logger)}
+  constructor(
+    private $fb: FirebaseService,
+    private $user: UserService,
+    protected $logger: LoggerService
+  ) {
+    super($logger);
+  }
 
   protected featureName = 'Friend';
 
-  protected resolveAction({action, id}: IFriendsEvent): Promise<any> {
+  protected resolveAction({ action, id, user }: IFriendsEvent): Promise<any> {
     return new Promise<any>((resolve) => {
       switch (action) {
         case Action.FetchRecommendList:
-              resolve(this.fetchRecommendList(id as string));
-              break;
+          resolve(this.fetchRecommendList(id as string));
+          break;
         case Action.FetchInviteList:
-              resolve(this.fetchInviteList(id as string));
-              break;
+          resolve(this.fetchInviteList(id as string));
+          break;
+        case Action.AddFriend:
+          resolve(
+            this.getUser(user).then(u => this.addFriend(u, id as string))
+          );
+          break;
       }
     });
-    }
+  }
 
   /**
    *  @description 其實只是拿到DB裡全部陌生人的資料
-   * */ 
-  private fetchRecommendList(uid: string): Promise<{key: string, value: IFriend}[]> {
-    return new Promise((resolve)=>
-      this.$fb.request('user').read()
-      .then((resFriends: {key: string, value: IFriend}[]) => {
-        resFriends.filter(resFriend => resFriend.key !== uid )
-        })
-      .then((resFriends: {key: string, value: IFriend}[]) => resolve(resFriends)));
+   */
+  private fetchRecommendList(uid: string): Promise<IFriend[]> {
+    return new Promise((resolve) =>
+      this.$fb.request('user').read().then((friends: { key: string, value: IFriend }[]) => {
+        return friends.filter(friend => friend.key !== uid);
+      })
+        .then((friends: { key: string, value: IFriend }[]) =>
+          resolve(friends.map(friend => friend.value))
+        )
+    );
   }
 
   /**
    * @description 拿到對user發出交友邀請的人的資料
    */
-  private fetchInviteList(uid: string): Promise<IFriend[]> {
+  private fetchInviteList(uid: string): Promise<IUser[]> {
     return new Promise((resolve) =>
       this.$fb.request('user').read(uid)
-      .then((user: IUser) => {
-        if ((<string[]>user.inviteAddFriend)?.length > 0) {
-          forkJoin(user.inviteAddFriend?.map(id =>
-            this.$fb.request('user').read$(id)
-          .subscribe((friends: {key: string, value: IFriend}[]) => {
-            console.log('fetchInviteList', )
-            resolve(friends.map(friend => friend.value))
-            }
-            ))
-          )
-        }}
-        )
-    )
+        .then((user: IUser) => {
+          if ((user.inviteAddFriends as string[])?.length > 0) {
+            forkJoin(user.inviteAddFriends?.map(id => this.$fb.request('user').read$(id)))
+              .subscribe((friends: any) => {
+                resolve(friends as IUser[]);
+              });
+          }
+          else {
+            resolve([]);
+          }
+        })
+    );
+  }
+
+  private addFriend(user: User, friendId: string): Promise<void> {
+    return new Promise<void>((resolve) => {
+      user.addFriends(friendId).then(hasUpdated => {
+        if (hasUpdated) {
+          this.$fb
+            .request('user')
+            .update({
+              friends: user.friends,
+              inviteAddFriends: user.inviteAddFriends
+            } as IUser, user.id)
+            .then(() => resolve());
+        } else {
+          alert('已經在好友裡嚕');
+          resolve();
+        }
+      });
+    });
+  }
+
+  private getUser(user?: User): Promise<User> {
+    return new Promise<User>(resolve => {
+      if (user) {
+        resolve(user);
+      } else {
+        this.$user.user$.pipe(take(1)).subscribe(_ => resolve(_ as User));
+      }
+    });
   }
 }
