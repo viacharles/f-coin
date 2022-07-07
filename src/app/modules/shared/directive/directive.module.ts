0c127bdb2effe81a2e95e8e22f46e6b009f5844c
charly <hr.g5studio@gmail.com>
2022-07-07 20:34:41 +0800

    增加拖曳上傳圖片

--- src/app/modules/shared/directive/directive.module.ts
+++ src/app/modules/shared/directive/directive.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollControlDirective } from './scroll-control.directive';
 import { DragMoveDirective } from './drag-move.directive';
+import { DropUploadDirective } from './drop-upload.directive';
 
 
 
 @NgModule({
-  declarations: [ScrollControlDirective, DragMoveDirective],
+  declarations: [ScrollControlDirective, DragMoveDirective, DropUploadDirective],
   exports: [ScrollControlDirective, DragMoveDirective],
   imports: [
     CommonModule
